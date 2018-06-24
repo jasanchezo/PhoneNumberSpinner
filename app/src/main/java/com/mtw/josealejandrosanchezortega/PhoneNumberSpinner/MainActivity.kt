d8354ee720e87ce6ef2ef3adf2786d9bfc8b3588
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-06-24 18:33:28 -0500

    ejercicio terminado

--- app/src/main/java/com/mtw/josealejandrosanchezortega/PhoneNumberSpinner/MainActivity.kt
+++ app/src/main/java/com/mtw/josealejandrosanchezortega/PhoneNumberSpinner/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mtw.josealejandrosanchezortega.PhoneNumberSpinner
 
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -11,29 +13,47 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
+    // private val TAG : String = MainActivity::class.simpleName.toString()
+
+    var mSpinnerLabel : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        label_spinner!!.setOnItemSelectedListener(this)
+        // CONTENIDO DESARROLLADO:
+        label_spinner.setOnItemSelectedListener(this)
 
-        var list_of_items = arrayOf("Item 1", "Item 2", "Item 3")
-        val aa = ArrayAdapter(this, label_spinner, list_of_items)
-        aa.setDropDownViewResource(label_spinner)
-        label_spinner.adapter(aa)
+        // SE CREA EL Adapter PARA EL SPINNER USANDO EL CONTENIDO EN arrya DEFINIDO EN EL RECURSO strings Y DEFINIÉNDOLO CON UN LAYOUT DEL ITEM DEL SPINNER COMO simple_spinner_item
+        var adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item)
+
+        // SE DEFINE UN LAYOUT PARA EL LISTADO DEL SPINNER
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // SE ASOCIA EL ADAPTER PREVIAMENTE CREADO Y VINCULADO CON LOS DATOS, PERO AHORA CON EL WIDGET Spinner DEFINIDO EN EL ACTIVITY
+        label_spinner.adapter = adapter
+
     }
 
     fun showText(view: View) {
-        if (editText_main != null)
-            Toast.makeText(this, editText_main.text.toString(), Toast.LENGTH_SHORT).show()
+        if (editText_main != null) {
+            // SE CONCATENA EL VALOR DEL EditText MAS EL CONTENIDO DEL SPINNER
+            val showString : String = editText_main.text.toString() + " - " + mSpinnerLabel
+
+            // SE MUESTRA EL TEXTO EN EL DIALOGO TOAST
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show()
+
+            // SE ACTUALIZA EL ESTASO DE LOS TEXTVIEW ABAJO
+            text_phonelabel.text = showString
+        }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
+        mSpinnerLabel = parent?.getItemAtPosition(position).toString()
+        showText(view!!)
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Log.d(TAG, getString(R.string.nothing_selected))
     }
 }
